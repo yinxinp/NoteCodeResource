e94f412561a60fa1a8367a4cd530a3aff2fe4c1b
潘阿峰 <yinxinpz@163.com>
2019-06-03 17:39:05 +0800

    navgation tabbar demo complete！

--- react-native/NavigationDemo/App.js
+++ react-native/NavigationDemo/App.js
@@ -1,49 +1,42 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+const HomeScreen = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Home</Text>
+  </View>
+);
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const SettingScreen = () => (
+  <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <Text>Settings</Text>
+  </View>
+);
+const defaultNavigationOptions = ({ navigation }) => ({
+  tabBarIcon: ({ focused, tintColor }) => {
+    const { routeName } = navigation.state;
+    let IconComponent = Ionicons;
+    let iconName;
+    if (routeName === "Home") {
+      iconName = `ios-information-circle${focused ? "" : "-outline"}`;
+    } else if (routeName === "Settings") {
+      iconName = `ios-options`;
+    }
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    return <IconComponent name={iconName} size={25} color={tintColor} />;
+  },
+  tabBarOptions: {
+    activTintColor: "tomato",
+    inactiveTintColor: "gray"
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Settings: SettingScreen
+  },
+  { defaultNavigationOptions }
+);
+export default createAppContainer(TabNavigator);
